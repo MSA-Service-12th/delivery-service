468769df84b2da91677d16cda14b9eb0b5aeb025
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-07 02:36:49 +0900

    feat: 배송 관련 JPA 리포지토리 추가

---
+++ src/main/java/com/loopang/deliveryservice/infrastructure/persistence/DeliveryRouteJpaRepository.java
@@ -0,0 +1,9 @@
+package com.loopang.deliveryservice.infrastructure.persistence;
+
+import com.loopang.deliveryservice.domain.entity.DeliveryRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DeliveryRouteJpaRepository extends JpaRepository<DeliveryRoute, UUID> {
+}
