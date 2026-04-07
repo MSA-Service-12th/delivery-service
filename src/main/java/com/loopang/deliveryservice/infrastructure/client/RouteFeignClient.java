5c2fc118be6019bbdc9e5f1e790213b0031b2a60
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-07 19:43:04 +0900

    ëfix: 누routeFeignClient 누락된 fallback 설정 추가

--- src/main/java/com/loopang/deliveryservice/infrastructure/client/RouteFeignClient.java
+++ src/main/java/com/loopang/deliveryservice/infrastructure/client/RouteFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "route-service", contextId = "routeFeignClient")
+@FeignClient(name = "route-service", contextId = "routeFeignClient", fallbackFactory = RouteFeignClientFallbackFactory.class)
 public interface RouteFeignClient {
 
 	@PostMapping("/api/hub-routes/calculate")
