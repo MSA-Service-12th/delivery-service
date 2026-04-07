3b80d3bce6b0684f2a0843f40e47ede6db9260eb
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-07 20:09:49 +0900

    ëfix: topic 프로퍼티 관련 유효성 검사 규칙 적용

--- src/main/java/com/loopang/deliveryservice/infrastructure/kafka/DeliveryTopicProperties.java
+++ src/main/java/com/loopang/deliveryservice/infrastructure/kafka/DeliveryTopicProperties.java
@@ -2,7 +2,9 @@ package com.loopang.deliveryservice.infrastructure.kafka;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "topics.delivery")
 public record DeliveryTopicProperties(
 
